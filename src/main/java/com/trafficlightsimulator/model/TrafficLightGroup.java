eff9fad26de32f70f1e2bf7faf953dbcdeab7461
manoj-bhaskaran <manoj.bhaskaran@gmail.com>
2024-11-10 22:37:02 +0530

    Replace System.out with logger in TrafficLightGroup for improved logging control

--- src/main/java/com/trafficlightsimulator/model/TrafficLightGroup.java
+++ src/main/java/com/trafficlightsimulator/model/TrafficLightGroup.java
@@ -2,8 +2,11 @@ package com.trafficlightsimulator.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TrafficLightGroup {
+    private static final Logger logger = Logger.getLogger(TrafficLightGroup.class.getName());
     private List<TrafficLight> lights;
 
     // Constructor
@@ -15,12 +18,14 @@ public class TrafficLightGroup {
     public void addTrafficLight(TrafficLight light) {
         if (light != null) {
             lights.add(light);
+            logger.log(Level.INFO, "Traffic light added: {0}", light);
         }
     }
 
     // Method to remove a traffic light from the group
     public void removeTrafficLight(TrafficLight light) {
         lights.remove(light);
+        logger.log(Level.INFO, "Traffic light removed: {0}", light);
     }
 
     // Method to set all lights in the group to a specified color (if they support color change)
@@ -28,8 +33,9 @@ public class TrafficLightGroup {
         for (TrafficLight light : lights) {
             try {
                 light.setColor(color);
+                logger.log(Level.INFO, "Set color {0} for light: {1}", new Object[]{color, light});
             } catch (IllegalArgumentException e) {
-                System.out.println("Cannot set color for light: " + e.getMessage());
+                logger.log(Level.WARNING, "Cannot set color for light: {0}", e.getMessage());
             }
         }
     }
@@ -38,6 +44,7 @@ public class TrafficLightGroup {
     public void setAllLightsState(State state) {
         for (TrafficLight light : lights) {
             light.setState(state);
+            logger.log(Level.INFO, "Set state {0} for light: {1}", new Object[]{state, light});
         }
     }
 
@@ -49,7 +56,8 @@ public class TrafficLightGroup {
     // Method to display the current state of each light in the group (for debugging)
     public void displayGroupStatus() {
         for (TrafficLight light : lights) {
-            System.out.println("Light Type: " + light.getType() + ", Color: " + light.getColor() + ", State: " + light.getState());
+            logger.log(Level.INFO, "Light Type: {0}, Color: {1}, State: {2}", 
+                       new Object[]{light.getType(), light.getColor(), light.getState()});
         }
     }
 }
